b03148df5543383b014fd246d78b04398a8b4227
Diyapatel234 <77798242+Diyapatel234@users.noreply.github.com>
2021-10-17 14:14:48 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+export default function App() {
+    return(
+      <View>
+        
+      </View>
+    );
+}
+
---
+++ screens/Prices.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export default class Prices extends Component{
+  render(){
+    return(
+      <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+      <Text>Prices</Text>
+      </View>
+    )
+  }
+}
